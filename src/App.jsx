1f2539f8b2baa93d86dcee3950fc57ebc4735187
cyber-darl <onosdarlington939@gmail.com>
2026-01-29 11:16:56 +0100

    vercel edit 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HeroActive from './components/sections/HeroActive';
 import ShowcaseSection from './components/sections/ShowcaseSection';
 import SoftSkills from './components/sections/SoftSkills';
 import Experience from './components/sections/Experience';
+import Tron from './components/sections/Tron';
 import TechStack from './components/sections/TechStack';
 import Testimonials from './components/sections/Testimonials';
 import Contact from './components/sections/Contact';
